c0f701bb600c45da4aae19b94db83c065533bd6b
o.joseph <o.joseph@gloops.com>
2013-08-09 11:51:01 +0900

    Positive, Negative + Extending the Gravity Mechanism
    Made that the polaziration can be changed when clicked.
    Now making the gravity more acurate with the raycast below the
    character. When change polarization the ray below checks for both
    positive and negative, before it was only checking for one.

--- Assets/character.cs
+++ Assets/character.cs
@@ -9,19 +9,30 @@ public class character : MonoBehaviour {
 	public Ray theRayTop;
 	public Ray theRayBottom;
 	
+	
+	
 	public enum polarState{
 		neutral,
 		red,
 		blue
 	}
 	
+
+	
 	//We create a polar state for the charater
-	public polarState thePolar = polarState.blue;
-	private int currPolarity = 0;
+	public polarState thePolar;
+	
+	//We create a polar var
+	private bool currPolarity = false;
+	
+	//Material References
+	string refBlueMat = "materials/pulseBlue";
+	string refRedMat = "materials/pulseRed";
+	
 	
 	// Use this for initialization
 	void Start () {
-		
+		thePolar = polarState.blue;
 
 	}
 	
@@ -41,7 +52,7 @@ public class character : MonoBehaviour {
 		
 		
 		
-		//We make a vector 3
+		//We make a vector 3 with the distance
 		Vector3 rayTopDist = new Vector3(0,10,0);
 		Vector3 rayBottomDist = new Vector3(0,-10,0);
 		
@@ -49,35 +60,99 @@ public class character : MonoBehaviour {
         Debug.DrawRay(this.transform.position, rayTopDist, Color.green); 
 		Debug.DrawRay(this.transform.position, rayBottomDist, Color.red); 
 		
-		if(Physics.Raycast(this.transform.position, rayBottomDist, out theHitBottom) == true){
-			//print("WE HIT SOMETHING "+ theHitBottom.transform.gameObject.name);
-			//rigidbody.AddForce (0,-10,0);
-			
-			if(theHitBottom.transform.gameObject.tag == "redTag"){
-				//rigidbody.AddForce (0,10,0);
-				//print("AAAAAA");
-				rigidbody.AddForce (0,-20,0);
-				if(thePolar == polarState.red){
-					print ("YELLOW");
-					rigidbody.AddForce (0,-60,0);
-				}
-			}
-		}
+		
+		
 		
 		if(Physics.Raycast(this.transform.position, rayTopDist, out theHitTop) == true){
 			//print("WE HIT SOMETHING "+ theHitTop.transform.gameObject.name);
-			//print(theHitTop.transform.gameObject.tag);
+			print(theHitTop.transform.gameObject.tag);
 			
 			if(theHitTop.transform.gameObject.tag == "blueTag"){
 				//rigidbody.AddForce (0,10,0);
 				//print("AAAAAA");
-				rigidbody.AddForce (0,20,0);
+				
+				
+				if(thePolar == polarState.blue){
+					//print ("YELLOW");
+					rigidbody.AddForce (0,20,0);
+				}else{
+					rigidbody.AddForce (0,-20,0);
+					
+				}
+				
+				
+				
 			}
 		}
 		
 		
+		
+		
+		
+		
+		
+		
+		
+		if(Physics.Raycast(this.transform.position, rayBottomDist, out theHitBottom) == true){
+			//print("WE HIT SOMETHING "+ theHitBottom.transform.gameObject.name);
+			//rigidbody.AddForce (0,-10,0);
+			
+			
+			
+			//We check if there is a redTag below, if so we react according to it
+			if(theHitBottom.transform.gameObject.tag == "redTag"){
+			
+				if(thePolar == polarState.red){
+					//We push it away
+					rigidbody.AddForce (0,20,0);
+				}else{
+					//We bring it closer 
+					rigidbody.AddForce (0,-20,0);
+				}
+			}
+			
+			
+			//We check if there is a blueTag below, if so we react according to it
+			if(theHitBottom.transform.gameObject.tag == "blueTag"){
+				
+				if(thePolar == polarState.blue){
+					//We push it away
+					rigidbody.AddForce (0,20,0);
+				}else{
+					//We bring it closer 
+					rigidbody.AddForce (0,-20,0);
+				}
+
+			}
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 	
+	
+	////////////////////////////////////////
+	//	On Collision
 	void OnCollisionEnter( Collision theCollision){
 		/*if(theCollision.gameObject.name == "theCeiling"){
 			print ("Collision: " + theCollision.gameObject.name);
@@ -85,7 +160,7 @@ public class character : MonoBehaviour {
 		switch(theCollision.gameObject.name){
 			case "death":
 				print("YOU DIED!");
-			this.renderer.material.color = Color.red;
+				this.renderer.material.color = Color.red;
 			break;
 		}
 		
@@ -93,17 +168,48 @@ public class character : MonoBehaviour {
 		
 	}
 	
+	
+	
+	
+	////////////////////////////////////////
+	//	On click
 	void OnMouseDown(){
-		print("mouseClick");
 		
-		//We change the polarity upon mouse click
-		currPolarity += 1;
-		if(currPolarity >= 3){
-			currPolarity =0;	
+		
+		//We change the polarity
+		currPolarity = !currPolarity;	
+		
+		
+		
+		//We check for the current polarity state
+		switch(currPolarity){
+			
+			//Becomes red
+			case true:					
+			
+				//We change rhe color on the fly
+				Material matRed = Resources.Load(refRedMat, typeof(Material)) as Material;		
+				this.renderer.material = matRed;
+				
+				//We change the polarState
+				thePolar = polarState.red;	
+			
+			break;
+			
+			
+			//Becomes blue
+			case false:				
+				//We change rhe color on the fly
+				Material matBlue = Resources.Load(refBlueMat, typeof(Material)) as Material;		
+				this.renderer.material = matBlue;
+				
+				//We change the polarState
+				thePolar = polarState.blue;	
+			
+			break;	
 		}
 		
-		this.renderer.material.color = Color.yellow;
-		thePolar = polarState.red;
+		
 	}
 	
 }
